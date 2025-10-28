e8e032c0edf59c8e6c85a09ee5a04a2bc9cbdc1f
theeunseojo <ces80372@naver.com>
2025-10-28 15:23:40 +0900

    프로그래머스 폰켓몬

--- src/programmers/hash02.java
+++ src/programmers/hash02.java
@@ -1,4 +1,25 @@
 package programmers;
 
-public class hash02 {
+import java.util.HashMap;
+
+class hash02 {
+    public int hash02(int[] nums) {
+        int answer = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        // 각 종류당 하나씩만 선택
+        for(int num : nums){
+            // 키가 존재하면
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            }
+            else{
+                map.put(num, 1);
+            }
+        }
+
+
+        answer = Math.min(nums.length/2, map.size());
+        return answer;
+    }
 }
